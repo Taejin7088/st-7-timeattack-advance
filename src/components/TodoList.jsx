23cf9cf153ea050d7409baf4c007c2a8d8380311
taejin7088 <gog70888@gmail.com>
2025-03-07 12:58:49 +0900

    todolist 완료

--- src/components/TodoList.jsx
+++ src/components/TodoList.jsx
@@ -1,7 +1,7 @@
-import { useQuery, useQueryClient } from "@tanstack/react-query";
-import { FaHeart, FaRegHeart } from "react-icons/fa";
-import { useNavigate } from "react-router-dom";
-import { todoApi } from "../api/todos";
+import { useMutation, useQuery, useQueryClient } from '@tanstack/react-query';
+import { FaHeart, FaRegHeart } from 'react-icons/fa';
+import { useNavigate } from 'react-router-dom';
+import { todoApi } from '../api/todos';
 
 export default function TodoList() {
   const navigate = useNavigate();
@@ -12,9 +12,9 @@ export default function TodoList() {
     isPending,
     refetch,
   } = useQuery({
-    queryKey: ["todos"],
+    queryKey: ['todos'],
     queryFn: async () => {
-      const response = await todoApi.get("/todos");
+      const response = await todoApi.get('/todos');
       return response.data;
     },
   });
@@ -22,23 +22,43 @@ export default function TodoList() {
   // TODO: 필수: 아래 handleLike 로 구현되어 있는 부분을 useMutation 으로 리팩터링 해보세요. 모든 기능은 동일하게 동작해야 합니다.
   // TODO: 선택: useMutation 으로 리팩터링 후, useTodoMutation 커스텀훅으로 정리해 보세요.
   const queryClient = useQueryClient();
-  const handleLike = async (id, currentLiked) => {
-    const previousTodos = [...todos];
-    try {
-      queryClient.setQueryData(["todos"], (prev) =>
-        prev.map((todo) =>
-          todo.id === id ? { ...todo, liked: !todo.liked } : todo,
-        ),
-      );
+
+  const mutation = useMutation({
+    mutationFn: async ({ id, currentLiked }) => {
       await todoApi.patch(`/todos/${id}`, {
         liked: !currentLiked,
       });
-    } catch (err) {
-      console.error(err);
-      queryClient.setQueryData(["todos"], previousTodos);
-    } finally {
-      refetch();
-    }
+    },
+
+    onMutate: async (target) => {
+      await queryClient.cancelQueries({ queryKey: ['todos'] });
+      const prev = queryClient.getQueryData(['todos']);
+      console.log(prev);
+      queryClient.setQueryData(['todos'], (old) => {
+        return old.map((todo) => {
+          if (todo.id === target.id) {
+            const tempTodo = { ...todo };
+            tempTodo.liked = !todo.liked;
+            return tempTodo;
+          }
+          return todo;
+        });
+      });
+
+      return { prev };
+    },
+
+    onError: (err, __, context) => {
+      console.log('삭제중에러발생', err);
+      queryClient.setQueryData(['todos'], context.prev);
+    },
+    onSettled: () => {
+      queryClient.invalidateQueries({ queryKey: ['todos'] });
+    },
+  });
+
+  const handleLike = async (id, currentLiked) => {
+    mutation.mutate({ id, currentLiked });
   };
 
   if (isPending) {
@@ -53,30 +73,30 @@ export default function TodoList() {
   }
 
   return (
-    <ul style={{ listStyle: "none", width: 250 }}>
+    <ul style={{ listStyle: 'none', width: 250 }}>
       {todos.map((todo) => (
         <li
           key={todo.id}
           style={{
-            border: "1px solid black",
-            padding: "10px",
-            marginBottom: "10px",
+            border: '1px solid black',
+            padding: '10px',
+            marginBottom: '10px',
           }}
         >
           <h3>{todo.title}</h3>
-          <div style={{ display: "flex", justifyContent: "space-between" }}>
+          <div style={{ display: 'flex', justifyContent: 'space-between' }}>
             <button onClick={() => navigate(`/detail/${todo.id}`)}>
               내용보기
             </button>
             {todo.liked ? (
               <FaHeart
                 onClick={() => handleLike(todo.id, todo.liked)}
-                style={{ cursor: "pointer" }}
+                style={{ cursor: 'pointer' }}
               />
             ) : (
               <FaRegHeart
                 onClick={() => handleLike(todo.id, todo.liked)}
-                style={{ cursor: "pointer" }}
+                style={{ cursor: 'pointer' }}
               />
             )}
           </div>
